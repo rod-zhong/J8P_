e29270797f97b92b805e034d37b6ad6b9a1ba567
Rod Zhong <rodz@synnex.com>
2018-11-26 19:07:05 +0800

    init

---
+++ src/main/java/rod/demo/j8p/J8pApplication.java
@@ -0,0 +1,12 @@
+package rod.demo.j8p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J8pApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(J8pApplication.class, args);
+    }
+}
---
+++ src/test/java/rod/demo/j8p/J8pApplicationTests.java
@@ -0,0 +1,16 @@
+package rod.demo.j8p;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class J8pApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
